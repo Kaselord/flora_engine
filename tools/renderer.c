e02c4c7d8ae29124382bff2cb3a23054c49154e4
Käselord <emil@richter-online.eu>
2024-05-05 00:08:09 +0200

    moved background drawing out of its function

--- tools/renderer.c
+++ tools/renderer.c
@@ -8,18 +8,21 @@ Uint8 bg_colour_r = 255;
 Uint8 bg_colour_g = 0;
 Uint8 bg_colour_b = 127;
 
-
-void draw_background(Uint8 r, Uint8 g, Uint8 b) {
-    SDL_SetRenderDrawColor(game_renderer, r, g, b, 255);
-    SDL_RenderFillRect(game_renderer, NULL);
-}
+struct Geometry {
+    SDL_Vertex vertices[64];
+    int vertex_count;
+};
 
 void initialize_rendering(void) {
-    game_window = SDL_CreateWindow("fig engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 640, 0);
+    game_window = SDL_CreateWindow("fire engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 640, 0);
 	game_renderer = SDL_CreateRenderer(game_window, 0, 0);
 }
 
 void final_render(void) {
-    draw_background(bg_colour_r, bg_colour_g, bg_colour_b);
+    // draw background
+    SDL_SetRenderDrawColor(game_renderer, bg_colour_r, bg_colour_g, bg_colour_b, 255);
+    SDL_RenderFillRect(game_renderer, NULL);
+
+    // render everything onto the window
     SDL_RenderPresent(game_renderer);
 }
