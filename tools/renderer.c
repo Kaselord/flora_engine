8ed1a87794b692c48010c9a1adf667bf004f8fba
Käselord <emil@richter-online.eu>
2024-05-05 19:41:58 +0200

    renderer stuffs

--- tools/renderer.c
+++ tools/renderer.c
@@ -8,10 +8,23 @@ Uint8 bg_colour_r = 255;
 Uint8 bg_colour_g = 0;
 Uint8 bg_colour_b = 127;
 
-struct Geometry {
-    SDL_Vertex vertices[64];
+typedef struct DrawablePolygon {
+    SDL_Vertex *vertices;
     int vertex_count;
-};
+    // optional!
+    SDL_Texture *texture;
+} DrawablePolygon;
+
+typedef struct DrawableRect {
+    Uint8 r;
+    Uint8 g;
+    Uint8 b;
+    Uint8 a;
+    SDL_Rect rect;
+} DrawableRect;
+
+DrawablePolygon draw_queue_polygons[64];
+DrawableRect draw_queue_rects[64];
 
 void initialize_rendering(void) {
     game_window = SDL_CreateWindow("fire engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 640, 0);
--- tools/renderer.h
+++ tools/renderer.h
@@ -4,5 +4,7 @@ Uint8 background_colour_r;
 Uint8 background_colour_g;
 Uint8 background_colour_b;
 
+void add_drawable_polygon();
+
 void initialize_rendering(void);
 void final_render(void);
